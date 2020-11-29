1809fd8592c6d56b8f907285529cada69b3abbc5
RebeccaHA <rebeccahunt034@gmail.com>
2020-11-29 19:37:03 +0000

    added two inputs, one showing every key stroke entered, the second only showing the data stored when hitting enter

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const app = Vue.createApp({
   data() {
-    return {};
+    return {
+      text: "",
+      textTwo: ""
+    };
   },
   methods: {
     showAlert() {
       alert("Boo!");
+    },
+    output(e) {
+      this.text = e.target.value;
+    },
+    outputEnter(e) {
+      this.textTwo = e.target.value;
     }
   }
 });
